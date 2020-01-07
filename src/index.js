b39ade2b7d6af1864d192930987d9c2b2b63466c
Andrew <applecidera@gmail.com>
2020-01-07 10:59:44 -0500

    added assets

--- src/index.js
+++ src/index.js
@@ -13,27 +13,33 @@ import './styles/index.scss';
 // console.log("hello");
 
 
-const sun = new Image();
-const moon = new Image();
-const earth = new Image();
+const sunMap = new Image();
+const cursor = new Image();
 let degrees = 0;
 
 function init() {
-  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
-  moon.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
-  earth.src = 'https://raw.githubusercontent.com/applecidera/convergence/master/src/assets/cursor.png';
+  sunMap.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+  cursor.src = 'https://raw.githubusercontent.com/applecidera/convergence/master/src/assets/cursor_rotated.png';
   window.requestAnimationFrame(draw);
 }
 
 let DIM_X = 768;
 let DIM_Y = 768;
 
-function draw() {
-  let canvas = document.getElementById('stage');
+let canvas = document.getElementById('stage');
   
-  canvas.width = DIM_X;
-  canvas.height = DIM_Y;
-  let ctx = canvas.getContext('2d');
+canvas.width = DIM_X;
+canvas.height = DIM_Y;
+let ctx = canvas.getContext('2d');
+
+document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  console.log(e.code);
+}
+
+function draw() {
+  degrees += 2;
 
   ctx.globalCompositeOperation = 'destination-over';
   ctx.clearRect(0, 0, DIM_X, DIM_Y); // clear canvas
@@ -43,12 +49,11 @@ function draw() {
   ctx.save();
   ctx.translate(DIM_X/2, DIM_Y/2);
 
-  // Earth
-  var time = new Date();
-  ctx.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
-  ctx.translate(105, 0);
-  ctx.fillRect(0, -12, 40, 24); // Shadow
-  ctx.drawImage(earth, -12, -12);
+  // Cursor
+  
+  ctx.rotate(((2 * Math.PI) / 360) * (degrees % 360));
+  ctx.translate(85, 0);
+  ctx.drawImage(cursor, -12, -12);
 
   ctx.restore();
   
@@ -56,7 +61,7 @@ function draw() {
   ctx.arc(DIM_X/2, DIM_Y/2, 105, 0, Math.PI * 2, false); // Earth orbit
   ctx.stroke();
  
-  ctx.drawImage(sun, 0, 0, DIM_X, DIM_Y);
+  ctx.drawImage(sunMap, 0, 0, DIM_X, DIM_Y);
 
   window.requestAnimationFrame(draw);
 }
